630c89aeecde83047625e18b043604521abf6353
muheebkamran <muheebkamran53@gmail.com>
2026-08-20 16:18:35 +0500

    function that multiplies two values if given if it is given one value it multiplies it with one else with the value given

---
+++ Lecture-7/Finger-exercise-book4.py
@@ -0,0 +1,13 @@
+# page no.95
+def mult(x,y = 1):
+    '''
+ Write a function mult that accepts either one or
+two ints as arguments. If called with two arguments, the function
+prints the product of the two arguments. If called with one argument,
+it prints that argument.
+
+    '''
+    ans = x * y 
+    return ans
+print(mult(9))
+print(mult(9,2))
