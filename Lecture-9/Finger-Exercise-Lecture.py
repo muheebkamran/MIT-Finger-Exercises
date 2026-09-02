9c1b0c147c1ab51ba411d88e02f92682f04e0980
muheebkamran <muheebkamran53@gmail.com>
2026-09-02 17:48:37 +0500

    Add problem set files for down payment calculations and binary search for interest rate

---
+++ Lecture-9/Finger-Exercise-Lecture.py
@@ -0,0 +1,19 @@
+def dot_product(tA, tB):
+    """
+    tA: a tuple of numbers
+    tB: a tuple of numbers of the same length as tA
+    Assumes tA and tB are the same length.
+    Returns a tuple where the:
+    * first element is the length of one of the tuples
+    * second element is the sum of the pairwise products of tA and tB
+    """
+    (lenght,mult_of_elt) = (0,0)
+    # Count elements while adding each pairwise product.
+    for i in range(len(tA)):
+        lenght += 1
+        mult_of_elt += tA[i] * tB[i]
+    return (lenght, mult_of_elt)
+# Examples:
+tA = (1, 2, 3)
+tB = (4, 5, 6)   
+print(dot_product(tA, tB)) # prints (3,32)
