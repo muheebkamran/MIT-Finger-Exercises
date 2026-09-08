ab26e332f3e80a6fd3cc6b437c75f858a6fae6b5
muheebkamran <muheebkamran53@gmail.com>
2026-09-08 19:09:36 +0500

    Add examples for string search and list manipulation in Finger-exercise-book.py

---
+++ Lecture-8/Finger-exercise-book.py
@@ -0,0 +1,7 @@
+# div = lambda x, y: x / y if y != 0 else None
+
+# print(div(10, 0))
+
+s = "abcbc"
+print(s.find("bc"))  # prints 1
+print(s.find("bc", 2))  # prints 3
---
+++ Lecture-9/Finger-exercise-book.py
@@ -0,0 +1,4 @@
+# Expression that evaluates to the mean of a tuple of numbers using function 'sum'. pg no.117
+tuple = (10, 20, 30, 40)
+mean_of_tuple = sum(tuple) / len(tuple)
+print(mean_of_tuple)
